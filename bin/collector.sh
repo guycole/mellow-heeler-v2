1e1ece61270dc74e911b92dfe5b16ad97e607693
Guy Cole <guycole@gmail.com>
2026-07-03 18:32:13 -0700

    logger tweak

--- bin/collector.sh
+++ bin/collector.sh
@@ -10,7 +10,7 @@
 PATH=/bin:/usr/bin:/etc:/usr/local/bin; export PATH
 #
 hostname=$(hostname)
-logger -p local3.info "collector $hostname"
+logger -p local3.info "collector heeler $hostname"
 #
 WORK_DIR="/home/wombat/github/mellow-heeler-v2/src/collector"
 #
