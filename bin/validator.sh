4132516a9eb2c69b182be784f0bc0584c295411b
Guy Cole <guycole@gmail.com>
2026-08-01 22:24:54 -0700

    validator script test

--- bin/validator.sh
+++ bin/validator.sh
@@ -7,12 +7,18 @@
 #
 PATH=/bin:/usr/bin:/etc:/usr/local/bin; export PATH
 #
+CONTAINER="heeler-validate"
+IMAGE="ghcr.io/guycole/wombat:latest"
+#
+WOMBAT_UID=$(id -u wombat)
+WOMBAT_GID=$(id -g wombat)
+#
 echo "start validate"
 #
-docker rm heeler-validate;docker run -v /var/wombat:/mnt/wombat --name heeler-validate heeler:latest
+docker rm ${CONTAINER};docker run -e WOMBAT_UID=${WOMBAT_UID} -e WOMBAT_GID=${WOMBAT_GID} -v /var/wombat:/mnt/wombat --name ${CONTAINER} ${IMAGE}
 #
-docker rm heeler-koala;docker run -e stuntbox=koala -v /var/wombat:/mnt/wombat --name heeler-koala heeler:latest
-$HOME/github/mellow-heeler-v2/bin/koala-import.sh
+#docker rm heeler-koala;docker run -e stuntbox=koala -e WOMBAT_UID=${WOMBAT_UID} -e WOMBAT_GID=${WOMBAT_GID} -v /var/wombat:/mnt/wombat --name heeler-koala heeler:latest
+#$HOME/github/mellow-heeler-v2/bin/koala-import.sh
 #
 echo "end validate"
 #
