ba295f446031f477a057bcbb485bfd05292e1b44
Guy Cole <guycole@gmail.com>
2026-09-08 13:39:38 -0700

    fix dockerfile

--- bin/validator.sh
+++ bin/validator.sh
@@ -13,9 +13,9 @@ IMAGE="ghcr.io/guycole/wombat-heeler:latest"
 #
 echo "start validate"
 #
-docker rm ${CONTAINER1};docker run -v /var/wombat:/mnt/wombat --name ${CONTAINER1} ${IMAGE}
+docker rm ${CONTAINER1};docker run --entrypoint python -v /var/wombat:/mnt/wombat --name ${CONTAINER1} ${IMAGE} heeler_app.py
 #
-docker rm ${CONTAINER2};docker run -e stuntbox=koala -v /var/wombat:/mnt/wombat --name ${CONTAINER2} ${IMAGE}
+docker rm ${CONTAINER2};docker run --entrypoint python -e stuntbox=koala -v /var/wombat:/mnt/wombat --name ${CONTAINER2} ${IMAGE} heeler_app.py
 $HOME/github/mellow-heeler-v2/bin/koala-import.sh
 #
 echo "end validate"
